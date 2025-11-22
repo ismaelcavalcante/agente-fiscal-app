648e5810fb6e2220f7da6a2f13106683f8f99bf4
ismaelcavalcante <ismael.agriroo@gmail.com>
2025-11-22 17:29:27 -0400

    fdsfsdfs

--- src/graph/builder.py
+++ src/graph/builder.py
@@ -1,5 +1,4 @@
 from langgraph.graph import StateGraph, END
-from graph.router import node_router
 from graph.nodes import (
     node_rag_qdrant,
     node_web_search,
@@ -8,7 +7,7 @@ from graph.nodes import (
 )
 from utils.logs import logger
 
-state_schema = dict  # simples e estável
+state_schema = dict
 
 def build_graph(llm, retriever, web_tool):
     logger.info("⛓️  Construindo LangGraph (versão profissional)...")
@@ -16,19 +15,19 @@ def build_graph(llm, retriever, web_tool):
     workflow = StateGraph(state_schema)
 
     # Nodes
-    workflow.add_node("router", lambda s: node_router(s))
+    workflow.add_node("router", node_router)
     workflow.add_node("rag_qdrant", lambda s: node_rag_qdrant(s, retriever))
     workflow.add_node("web_search", lambda s: node_web_search(s, web_tool))
     workflow.add_node("direct_answer", lambda s: node_direct_answer(s, llm))
     workflow.add_node("generate_final", lambda s: node_generate_final(s, llm))
 
-    # Start
+    # Entry point
     workflow.set_entry_point("router")
 
-    # Correct conditional routing
+    # Roteamento correto
     workflow.add_conditional_edges(
         "router",
-        lambda state: state["__route__"],   # <- ROTEAMENTO CORRETO
+        lambda state: state["__route__"],
         {
             "RAG": "rag_qdrant",
             "WEB": "web_search",
@@ -39,7 +38,6 @@ def build_graph(llm, retriever, web_tool):
     workflow.add_edge("rag_qdrant", "generate_final")
     workflow.add_edge("web_search", "generate_final")
     workflow.add_edge("direct_answer", "generate_final")
-
     workflow.add_edge("generate_final", END)
 
     graph = workflow.compile()
